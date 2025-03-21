7f9c832acdecff1aef5abcd04f293548104bf868
pramod <gopinath.pramod@gmail.com>
2025-03-21 15:57:12 +0530

    Committ

--- gym_bot/settings.py
+++ gym_bot/settings.py
@@ -160,7 +160,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
+AWS_ACCESS_KEY_ID = "AKIAWLVNT2SPVAOHJHFM"
+AWS_SECRET_ACCESS_KEY = "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"
 AWS_STORAGE_BUCKET_NAME = 'johnsconcept'
 AWS_S3_SIGNATURE_NAME = 's3v4',
 AWS_S3_REGION_NAME = 'ap-south-1'
